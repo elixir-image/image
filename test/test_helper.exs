f0c456f33946ec4ff36973390ec5dd634435cc8f
Kip Cole <kipcole9@gmail.com>
2022-05-19 23:29:42 +0800

    Clean out old test failed matches each test run

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-paths = Path.wildcard("test/spport/did_not_match/**/*.{png,tif,jpg}")
+paths = Path.wildcard("test/support/did_not_match/**/*.{png,tif,jpg}")
 Enum.each(paths, &File.rm/1)
 
 ExUnit.configure(exclude: [full: true])
