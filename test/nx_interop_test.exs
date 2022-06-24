6c2f85589b6e8fc9014879f6428a1bf51dbb4d94
Kip Cole <kipcole9@gmail.com>
2022-06-24 18:46:42 +0800

    Don't run Nx tests if Nx is not available

--- test/nx_interop_test.exs
+++ test/nx_interop_test.exs
@@ -1,57 +1,58 @@
-defmodule Image.Nx.Test do
-  use ExUnit.Case, async: true
-  import Image.TestSupport
+if Code.ensure_loaded?(Nx) do
+  defmodule Image.Nx.Test do
+    use ExUnit.Case, async: true
+    import Image.TestSupport
 
-  test "An image can be used in Nx as a tensor" do
-    {:ok, image} = Vix.Vips.Operation.black(3, 3)
-    {:ok, tensor} = Vix.Vips.Image.write_to_tensor(image)
+    test "An image can be used in Nx as a tensor" do
+      {:ok, image} = Vix.Vips.Operation.black(3, 3)
+      {:ok, tensor} = Vix.Vips.Image.write_to_tensor(image)
 
-    %Vix.Tensor{data: binary, names: names, shape: shape, type: type} = tensor
+      %Vix.Tensor{data: binary, names: names, shape: shape, type: type} = tensor
 
-    assert Nx.tensor([
-        [
-          [0],
-          [0],
-          [0]
-        ],
-        [
-          [0],
-          [0],
-          [0]
-        ],
-        [
-          [0],
-          [0],
-          [0]
-        ]
-        ],
-        type: {:u, 8},
-        names: names) ==
-      binary
-      |> Nx.from_binary(type)
-      |> Nx.reshape(shape, names: names)
+      assert Nx.tensor([
+          [
+            [0],
+            [0],
+            [0]
+          ],
+          [
+            [0],
+            [0],
+            [0]
+          ],
+          [
+            [0],
+            [0],
+            [0]
+          ]
+          ],
+          type: {:u, 8},
+          names: names) ==
+        binary
+        |> Nx.from_binary(type)
+        |> Nx.reshape(shape, names: names)
 
+    end
+
+    test "That an image round trips to Nx and back" do
+      image = image_path("Kamchatka-2019-8754.jpg")
+
+      {:ok, image} = Image.open(image, access: :random)
+      {:ok, tensor} = Image.to_nx(image)
+      {:ok, image2} = Image.from_nx(tensor)
+
+      assert_images_equal image, image2
+    end
+
+    test "That an image round trips to Nx and back with transposition" do
+      image = image_path("Kamchatka-2019-8754.jpg")
+
+      {:ok, image} = Image.open(image, access: :random)
+      {:ok, tensor} = Image.to_nx(image)
+      tensor = Nx.transpose(tensor, axes: [:height, :width, :bands])
+      {:ok, image2} = Image.from_nx(tensor)
+
+      assert_images_equal image, image2
+    end
   end
-
-  test "That an image round trips to Nx and back" do
-    image = image_path("Kamchatka-2019-8754.jpg")
-
-    {:ok, image} = Image.open(image, access: :random)
-    {:ok, tensor} = Image.to_nx(image)
-    {:ok, image2} = Image.from_nx(tensor)
-
-    assert_images_equal image, image2
-  end
-
-  test "That an image round trips to Nx and back with transposition" do
-    image = image_path("Kamchatka-2019-8754.jpg")
-
-    {:ok, image} = Image.open(image, access: :random)
-    {:ok, tensor} = Image.to_nx(image)
-    tensor = Nx.transpose(tensor, axes: [:height, :width, :bands])
-    {:ok, image2} = Image.from_nx(tensor)
-
-    assert_images_equal image, image2
-  end
-
 end
