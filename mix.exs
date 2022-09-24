a5f3d7619a0312f8792318c1f8143dcf78a17798
Kip Cole <kipcole9@gmail.com>
2022-09-24 15:30:31 +0800

    Add evision as a dependency

--- mix.exs
+++ mix.exs
@@ -51,7 +51,10 @@ defmodule Image.MixProject do
       # {:vix, path: "../vix"},
 
       # eVision OpenCV bindings
-      if(dev?(), do: {:evision, "~> 0.1.4", github: "cocoa-xu/evision", tag: "v0.1.6"}),
+      if(dev?(), do: {:evision, "~> 0.1.6", github: "cocoa-xu/evision", tag: "v0.1.6"}),
+
+      # eVision
+      {:evision, "~> 0.1.4", github: "cocoa-xu/evision", tag: "v0.1.4"},
 
       {:sweet_xml, "~> 0.7"},
       {:phoenix_html, "~> 3.2 or ~> 2.14"},
