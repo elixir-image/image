bd9d239bcdf57e0039fcf6a8a69c90c9a1ea4553
Kip Cole <kipcole9@gmail.com>
2023-01-08 06:35:03 +0100

    mix format

--- lib/image/video.ex
+++ lib/image/video.ex
@@ -21,10 +21,12 @@ if match?({:module, _module}, Code.ensure_compiled(Evision)) do
     alias Vix.Vips.Image, as: Vimage
 
     @doc "Guards that a frame offset is valid for a video"
-    defguard is_frame(frame, frame_count) when is_integer(frame) and frame in 0..(trunc(frame_count) - 1)
+    defguard is_frame(frame, frame_count)
+             when is_integer(frame) and frame in 0..(trunc(frame_count) - 1)
 
     @doc "Guards that a millisecond count is valid for a video"
-    defguard is_valid_millis(millis, frames, fps) when is_integer(millis) and millis in 0..(trunc(fps * frames * 1000) - 1)
+    defguard is_valid_millis(millis, frames, fps)
+             when is_integer(millis) and millis in 0..(trunc(fps * frames * 1000) - 1)
 
     @doc """
     Opens a video file, calls the given function with the video
@@ -47,8 +49,8 @@ if match?({:module, _module}, Code.ensure_compiled(Evision)) do
         ...> end
 
     """
-    @spec with_video(filename :: Path.t, (Evision.VideoCapture.t() -> any())) ::
-      {:ok, Evision.VideoCapture.t()} | {:error, Image.error_message()}
+    @spec with_video(filename :: Path.t(), (Evision.VideoCapture.t() -> any())) ::
+            {:ok, Evision.VideoCapture.t()} | {:error, Image.error_message()}
 
     def with_video(filename, fun) when is_binary(filename) and is_function(fun, 1) do
       filename
@@ -89,8 +91,9 @@ if match?({:module, _module}, Code.ensure_compiled(Evision)) do
       case Evision.VideoCapture.videoCapture(filename) do
         %Evision.VideoCapture{} = video ->
           {:ok, video}
+
         error ->
-          {:error, "Could not open video #{inspect filename}. Error #{inspect error}"}
+          {:error, "Could not open video #{inspect(filename)}. Error #{inspect(error)}"}
       end
     end
 
@@ -140,13 +143,15 @@ if match?({:module, _module}, Code.ensure_compiled(Evision)) do
         iex> Image.Video.close(video)
 
     """
-    @spec close(Evision.VideoCapture.t()) :: {:ok, Evision.VideoCapture.t()} | {:error, Image.error_message()}
+    @spec close(Evision.VideoCapture.t()) ::
+            {:ok, Evision.VideoCapture.t()} | {:error, Image.error_message()}
     def close(%Evision.VideoCapture{} = video) do
       case Evision.VideoCapture.release(video) do
         %Evision.VideoCapture{} = video ->
           {:ok, video}
+
         error ->
-          {:error, "Could not close video. Error #{inspect error}"}
+          {:error, "Could not close video. Error #{inspect(error)}"}
       end
     end
 
@@ -202,18 +207,24 @@ if match?({:module, _module}, Code.ensure_compiled(Evision)) do
         iex> {:error, "Frame number must be in the range 0..172. Found 500"} = Image.Video.image_from_frame(video, 500)
 
     """
-    @spec image_from_frame(Evision.VideoCapture.t(), non_neg_integer()) :: {:ok, Vimage.t()} | {:error, Image.error_message()}
-    def image_from_frame(%Evision.VideoCapture{isOpened: true, frame_count: frame_count} = video, frame) when is_frame(frame, frame_count) do
-      with true <- Evision.VideoCapture.set(video, Evision.cv_CAP_PROP_POS_FRAMES, frame),
+    @spec image_from_frame(Evision.VideoCapture.t(), non_neg_integer()) ::
+            {:ok, Vimage.t()} | {:error, Image.error_message()}
+    def image_from_frame(
+          %Evision.VideoCapture{isOpened: true, frame_count: frame_count} = video,
+          frame
+        )
+        when is_frame(frame, frame_count) do
+      with true <- Evision.VideoCapture.set(video, Evision.cv_CAP_PROP_POS_FRAMES(), frame),
            %Evision.Mat{} = cv_image <- Evision.VideoCapture.read(video) do
         Image.from_evision(cv_image)
       else
-        _error -> {:error, "Could not extract the frame number #{inspect frame}."}
+        _error -> {:error, "Could not extract the frame number #{inspect(frame)}."}
       end
     end
 
     def image_from_frame(%Evision.VideoCapture{isOpened: true, frame_count: max_frame}, frame) do
-      {:error, "Frame number must be in the range 0..#{trunc(max_frame) - 1}. Found #{inspect frame}"}
+      {:error,
+       "Frame number must be in the range 0..#{trunc(max_frame) - 1}. Found #{inspect(frame)}"}
     end
 
     def image_from_frame(%Evision.VideoCapture{isOpened: false}, _frame) do
@@ -273,21 +284,29 @@ if match?({:module, _module}, Code.ensure_compiled(Evision)) do
         ...>   Image.Video.image_at_millisecond(video, 10_000_000)
 
     """
-    @spec image_at_millisecond(Evision.VideoCapture.t(), non_neg_integer()) :: {:ok, Vimage.t()} | {:error, Image.error_message()}
-    def image_at_millisecond(%Evision.VideoCapture{isOpened: true, frame_count: frame_count, fps: fps} = video, millis)
+    @spec image_at_millisecond(Evision.VideoCapture.t(), non_neg_integer()) ::
+            {:ok, Vimage.t()} | {:error, Image.error_message()}
+    def image_at_millisecond(
+          %Evision.VideoCapture{isOpened: true, frame_count: frame_count, fps: fps} = video,
+          millis
+        )
         when is_valid_millis(millis, frame_count, fps) do
-      with true <- Evision.VideoCapture.set(video, Evision.cv_CAP_PROP_POS_MSEC, millis),
+      with true <- Evision.VideoCapture.set(video, Evision.cv_CAP_PROP_POS_MSEC(), millis),
            %Evision.Mat{} = cv_image <- Evision.VideoCapture.read(video) do
         Image.from_evision(cv_image)
       else
-        _error -> {:error, "Could not extract the frame at #{inspect millis}."}
+        _error -> {:error, "Could not extract the frame at #{inspect(millis)}."}
       end
     end
 
-    def image_at_millisecond(%Evision.VideoCapture{isOpened: true, frame_count: frame_count, fps: fps}, millis) do
+    def image_at_millisecond(
+          %Evision.VideoCapture{isOpened: true, frame_count: frame_count, fps: fps},
+          millis
+        ) do
       max_millis = trunc(fps * frame_count * 1000) - 1
 
-      {:error, "Milliseconds number must be in the range 0..#{max_millis}. Found #{inspect millis}"}
+      {:error,
+       "Milliseconds number must be in the range 0..#{max_millis}. Found #{inspect(millis)}"}
     end
 
     def image_at_millisecond(%Evision.VideoCapture{isOpened: false}, _millis) do
@@ -312,7 +331,8 @@ if match?({:module, _module}, Code.ensure_compiled(Evision)) do
     * raises an exception.
 
     """
-    @spec image_at_millisecond!(Evision.VideoCapture.t(), non_neg_integer()) :: Vimage.t() | no_return()
+    @spec image_at_millisecond!(Evision.VideoCapture.t(), non_neg_integer()) ::
+            Vimage.t() | no_return()
     def image_at_millisecond!(%Evision.VideoCapture{} = video, millis) do
       case image_at_millisecond(video, millis) do
         {:ok, image} -> image
--- test/video_test.exs
+++ test/video_test.exs
@@ -5,12 +5,11 @@ defmodule Image.VideoTest do
   doctest Image.Video
 
   test "Extract a frame from a video" do
-    validate_path =  validate_path("video/video_sample_frame_0.png")
+    validate_path = validate_path("video/video_sample_frame_0.png")
 
     {:ok, video} = Image.Video.open("./test/support/video/video_sample.mp4")
     {:ok, image} = Image.Video.image_at_millisecond(video, 0)
 
     assert_images_equal(image, validate_path)
   end
-
 end
