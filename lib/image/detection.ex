ea111d4437e4160fa38768bf34595729afdc70a6
Kip Cole <kipcole9@gmail.com>
2023-03-19 17:06:09 +0800

    Move classes to their own file

--- lib/image/detection.ex
+++ lib/image/detection.ex
@@ -224,90 +224,14 @@ defmodule Image.Detection do
     |> Enum.take_while(fn [_, _, _, _, prob] -> prob > thresh end)
   end
 
+  @classes :image
+           |> Application.app_dir("priv/models/")
+           |> Path.join("coco_classes.txt")
+           |> File.read!
+           |> String.split("\n")
+           |> Enum.map(&String.trim/1)
+
   def classes do
-    """
-    person
-    bicycle
-    car
-    motorbike
-    aeroplane
-    bus
-    train
-    truck
-    boat
-    traffic light
-    fire hydrant
-    stop sign
-    parking meter
-    bench
-    bird
-    cat
-    dog
-    horse
-    sheep
-    cow
-    elephant
-    bear
-    zebra
-    giraffe
-    backpack
-    umbrella
-    handbag
-    tie
-    suitcase
-    frisbee
-    skis
-    snowboard
-    sports ball
-    kite
-    baseball bat
-    baseball glove
-    skateboard
-    surfboard
-    tennis racket
-    bottle
-    wine glass
-    cup
-    fork
-    knife
-    spoon
-    bowl
-    banana
-    apple
-    sandwich
-    orange
-    broccoli
-    carrot
-    hot dog
-    pizza
-    donut
-    cake
-    chair
-    sofa
-    pottedplant
-    bed
-    diningtable
-    toilet
-    tvmonitor
-    laptop
-    mouse
-    remote
-    keyboard
-    cell phone
-    microwave
-    oven
-    toaster
-    sink
-    refrigerator
-    book
-    clock
-    vase
-    scissors
-    teddy bear
-    hair drier
-    toothbrush
-    """
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
+    @classes
   end
 end
