0875e80bcd7f02b881f124e6e6bcc77a04285ad9
Kip Cole <kipcole9@gmail.com>
2023-04-05 11:10:12 +0800

    Add Image.crop/2 that takes the same bounding box format as Image.warp_perspective/3

--- test/perspective_test.exs
+++ test/perspective_test.exs
@@ -2,31 +2,43 @@ defmodule Image.Perspective.Test do
   use ExUnit.Case, async: true
   import Image.TestSupport
 
-  test "warps an image to perspective" do
-    original = image_path("warp_perspective.jpg")
-    expected = validate_path("warp_perspective.jpg")
+  @warp_from [
+         {139, 125},
+         {826, 74},
+         {796, 559},
+         {155, 483}
+       ]
 
-    {:ok, image} = Image.open(original, access: :random)
-
-    {:ok, result} =
-      Image.warp_perspective(
-        image,
-        [
+  @warp_to [
           {139, 125},
-          {826, 74},
-          {796, 559},
-          {155, 483}
-        ],
-        [
-          {139, 125},
-          {816, 125},
+          {815, 125},
           {815, 528},
-          {136, 528}
+          {139, 528}
         ]
-      )
 
+  test "warps an image to perspective" do
+    image_path = image_path("warp_perspective.jpg")
+    validate_path = validate_path("warp_perspective.jpg")
+
+    {:ok, image} = Image.open(image_path)
+    {:ok, result} = Image.warp_perspective(image, @warp_from, @warp_to)
+
+    # Image.write! result, validate_path
     {:ok, result} = Vix.Vips.Image.write_to_buffer(result, ".jpg")
 
-    assert_images_equal(result, expected)
+    assert_images_equal(result, validate_path)
+  end
+
+  test "post-crop of a warped image" do
+    image_path = image_path("warp_perspective.jpg")
+    validate_path = validate_path("warp_perspective_cropped.png")
+
+    {:ok, image} = Image.open(image_path)
+    {:ok, result} = Image.warp_perspective(image, @warp_from, @warp_to)
+
+    {:ok, cropped} = Image.crop(result, @warp_to)
+
+    # Image.write! cropped, validate_path
+    assert_images_equal(cropped, validate_path)
   end
 end
