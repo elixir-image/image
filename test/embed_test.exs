86186aa003ab1adc47a02886897f3913acf57391
Kip Cole <kipcole9@gmail.com>
2023-09-09 07:10:45 +1000

    Add tests for Image.embed/4

---
+++ test/embed_test.exs
@@ -0,0 +1,34 @@
+defmodule Image.Embed.Test do
+  use ExUnit.Case, async: true
+  import Image.TestSupport
+
+  test "Image.embed/4 with extend_mode: :black" do
+    image_file = "penguin_with_alpha.png"
+    validate_file = "embed/penguin_with_alpha_embed_black.png"
+
+    image_path = image_path(image_file)
+    validate_path = validate_path(validate_file)
+
+    image = Image.open!(image_path, access: :random)
+    {width, height, _bands} = Image.shape image
+    {:ok, embedded} = Image.embed(image, width, height + 50, x: 0, y: 0, extend_mode: :black)
+
+    # {:ok, _image} = Image.write(embedded, validate_path)
+    assert_images_equal(embedded, validate_path)
+  end
+
+  test "Image.embed/4 with extend_mode: :black, background_transparency: :transparent" do
+    image_file = "penguin_with_alpha.png"
+    validate_file = "embed/penguin_with_alpha_embed_black_transparent.png"
+
+    image_path = image_path(image_file)
+    validate_path = validate_path(validate_file)
+
+    image = Image.open!(image_path, access: :random)
+    {width, height, _bands} = Image.shape image
+    {:ok, embedded} = Image.embed(image, width, height + 50, x: 0, y: 0, extend_mode: :black, background_transparency: :transparent)
+
+    # {:ok, _image} = Image.write(embedded, validate_path)
+    assert_images_equal(embedded, validate_path)
+  end
+end
