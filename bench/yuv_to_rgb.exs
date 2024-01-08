74eb86f3937eda2de1337a21fc48ad2907f1d459
Kip Cole <kipcole9@gmail.com>
2024-01-09 07:03:48 +0800

    Add .yuv test image

--- bench/yuv_to_rgb.exs
+++ bench/yuv_to_rgb.exs
@@ -1,16 +1,10 @@
-raw_yuv_file = Path.expand("/Users/kip/Desktop/image.yuv")
-{:ok, f} = File.open(raw_yuv_file)
-data = IO.binread(f, :all)
-decoded = Image.YUV.decode(data, 1920, 1080, :C420)
+raw_yuv_file = Path.expand("./test/support/images/image.yuv")
+{:ok, binary} = File.read(raw_yuv_file)
 
 Benchee.run(
   %{
-    "Converting YUV to RGB" => fn ->
-      {:ok, i} = Image.YUV.to_rgb(decoded, 1920, 1080, :C420, :bt601)
-    end,
-    "Decoding and then converting YUV to RGB" => fn ->
-      decoded = Image.YUV.decode(data, 1920, 1080, :C420)
-      {:ok, i} = Image.YUV.to_rgb(decoded, 1920, 1080, :C420, :bt601)
+    "Converting YUV binary to an RGB image" => fn ->
+      {:ok, _i} = Image.YUV.new_from_binary(binary, 1920, 1080, :C420, :bt601)
     end,
     },
   time: 20,
--- lib/image/yuv.ex
+++ lib/image/yuv.ex
@@ -10,6 +10,7 @@ defmodule Image.YUV do
 
   # See https://mymusing.co/bt601-yuv-to-rgb-conversion-color/
   # And https://github.com/libvips/libvips/discussions/2561
+
   @bt601_to_rgb [
     [1.0,  0.0,       1.402   ],
     [1.0, -0.344136, -0.714136],
@@ -17,54 +18,100 @@ defmodule Image.YUV do
   ]
 
   # See https://mymusing.co/bt-709-yuv-to-rgb-conversion-color/
+
   @bt709_to_rgb [
     [1.0,  0.0,       1.5748  ],
     [1.0, -0.187324, -0.468124],
     [1.0,  1.8556,    0.0     ],
   ]
 
+  @valid_encodings [:C444, :C422, :C420]
+  @valid_colorspace [:bt601, :bt709]
+
   @doc """
-  Takes a binary and decodes to the `[y, u, v]` planes/
+  Converts the raw YUV data in a `.yuv` file
+  into an RGB image.
+
+  ### Arguments
+
+  * `path` is any accessible file system path
+
+  * `width` is the width of the image encoded in
+    the YUV data.
+
+  * `height` is the height of the image encoded in
+    the YUV data.
+
+  * `encoding` is one of `:C444`, `:C422` or
+    `:C420`.
+
+  * `colorspace is one of `:bt601` (the default) or
+    `:bt709`.
+
+  ### Returns
+
+  * `{:ok, rgb_image}` or
+
+  * `{:error, reason}`.
 
   """
-  def decode(binary, width, height, encoding \\ :C420)
+  @spec new_from_file(path :: Path.t, width :: pos_integer(), height :: pos_integer(), encoding :: atom(), colorspace :: atom()) ::
+    {:ok, Vimage.t()} | {:error, Image.error_message()}
 
-  def decode(binary, width, height, :C444) do
-    y_bytes = width * height
-
-    <<y::bytes-size(y_bytes), u::bytes-size(y_bytes), v::bytes-size(y_bytes)>> = binary
-    [y, u, v]
+  def new_from_file(path, width, height, encoding, colorspace \\ :bt601)
+      when encoding in @valid_encodings and colorspace in @valid_colorspace do
+    with {:ok, binary} <- File.read(path) do
+      new_from_binary(binary, width, height, encoding, colorspace)
+    end
   end
 
-  def decode(binary, width, height, :C420) do
-    y_bytes = width * height
-    uv_bytes = div(y_bytes, 4)
+  @doc """
+  Converts raw YUV data into an RGB image.
 
-    <<y::bytes-size(y_bytes), u::bytes-size(uv_bytes), v::bytes-size(uv_bytes)>> = binary
-    [y, u, v]
-  end
+  ### Arguments
 
-  def decode(binary, width, height, :C422) do
-    y_bytes = width * height
-    uv_bytes = div(y_bytes, 2)
+  * `binary` is raw YUV data as a binary.
 
-    <<y::bytes-size(y_bytes), u::bytes-size(uv_bytes), v::bytes-size(uv_bytes)>> = binary
-    [y, u, v]
+  * `width` is the width of the image encoded in
+    the YUV data.
+
+  * `height` is the height of the image encoded in
+    the YUV data.
+
+  * `encoding` is one of `:C444`, `:C422` or
+    `:C420`.
+
+  * `colorspace is one of `:bt601` (the default) or
+    `:bt709`.
+
+  ### Returns
+
+  * `{:ok, rgb_image}` or
+
+  * `{:error, reason}`.
+
+  """
+  @spec new_from_binary(binary :: binary(), width :: pos_integer(), height :: pos_integer(), encoding :: atom(), colorspace :: atom()) ::
+    {:ok, Vimage.t()} | {:error, Image.error_message()}
+
+  def new_from_binary(binary, width, height, encoding, colorspace \\ :bt601)
+      when encoding in @valid_encodings and colorspace in @valid_colorspace do
+    with {:ok, decoded} = decode(binary, width, height, encoding) do
+      to_rgb(decoded, width, height, encoding, colorspace)
+    end
   end
 
   @doc """
   Take an image in a YUV colorspace (which libvips does not
   understand) and convert it to RGB.
 
-  See https://github.com/libvips/libvips/discussions/2561
-
-  THIS IS NOT CURRENTLY PRODUCING THE CORRECT OUTPUT.
-
   """
+  # See https://github.com/libvips/libvips/discussions/2561
+
   def to_rgb(%Vimage{} = image, :bt601) do
     with {:ok, transform} <- Vimage.new_from_list(@bt601_to_rgb),
-         {:ok, float} <- Operation.recomb(image, transform),
-         {:ok, rgb} <- Image.cast(float, {:u, 8}) do
+         {:ok, recombed} <- Operation.recomb(image, transform),
+         {:ok, rgb} <- Image.cast(recombed, {:u, 8}) do
       Operation.copy(rgb, interpretation: :VIPS_INTERPRETATION_sRGB)
     end
   end
@@ -78,36 +125,87 @@ defmodule Image.YUV do
   end
 
   @doc """
-  Takes the `[y, u, v]` planes and convert to
-  and RGB image.
+  Takes the `[y, u, v]` planes and converts them to
+  an RGB image.
 
   """
   def to_rgb([y, u, v], width, height, :C444, colorspace) do
+    use Image.Math
+
     with {:ok, y} <- new_scaled_image(y, width, height, 1.0, 1.0),
          {:ok, u} <- new_scaled_image(u, width, height, 1.0, 1.0),
          {:ok, v} <- new_scaled_image(v, width, height, 1.0, 1.0),
          {:ok, image_444} <- Operation.bandjoin([y, u, v]) do
-      to_rgb(image_444, colorspace)
+      to_rgb(image_444 - [16, 128, 128], colorspace)
     end
   end
 
   def to_rgb([y, u, v], width, height, :C422, colorspace) do
+    use Image.Math
+
     with {:ok, y} <- new_scaled_image(y, width, height, 1.0, 1.0),
          {:ok, u} <- new_scaled_image(u, width, height, 2.0, 1.0),
          {:ok, v} <- new_scaled_image(v, width, height, 2.0, 1.0),
          {:ok, image_444} <- Operation.bandjoin([y, u, v]) do
-      to_rgb(image_444, colorspace)
+      to_rgb(image_444 - [16, 128, 128], colorspace)
     end
   end
 
   def to_rgb([y, u, v], width, height, :C420, colorspace) do
+    use Image.Math
+
     with {:ok, y} <- new_scaled_image(y, width, height, 1.0, 1.0),
          {:ok, u} <- new_scaled_image(u, width, height, 2.0, 2.0),
          {:ok, v} <- new_scaled_image(v, width, height, 2.0, 2.0),
          {:ok, image_444} <- Operation.bandjoin([y, u, v]) do
-      to_rgb(image_444, colorspace)
+      to_rgb(image_444 - [16, 128, 128], colorspace)
     end
   end
+
+  @doc """
+  Takes a raw YUV binary and decodes to the `[y, u, v]` planes.
+
+  """
+  def decode(binary, width, height, :C444) do
+    y_bytes = width * height
+
+    case binary do
+      <<y::bytes-size(y_bytes), u::bytes-size(y_bytes), v::bytes-size(y_bytes)>> ->
+        {:ok, [y, u, v]}
+
+      _other ->
+        {:error, "Could not decode raw YUV data as 4:4:4"}
+    end
+  end
+
+  def decode(binary, width, height, :C420) do
+    y_bytes = width * height
+    uv_bytes = div(y_bytes, 4)
+
+    case binary do
+      <<y::bytes-size(y_bytes), u::bytes-size(uv_bytes), v::bytes-size(uv_bytes)>> ->
+        {:ok, [y, u, v]}
+
+      _other ->
+        {:error, "Could not decode raw YUV data as 4:2:0"}
+    end
+  end
+
+  def decode(binary, width, height, :C422) do
+    y_bytes = width * height
+    uv_bytes = div(y_bytes, 2)
+
+    case binary do
+      <<y::bytes-size(y_bytes), u::bytes-size(uv_bytes), v::bytes-size(uv_bytes)>> ->
+        {:ok, [y, u, v]}
+
+      _other ->
+        {:error, "Could not decode raw YUV data as 4:2:2"}
+    end
+  end
+
+  # Scales the U and V planes to be the same size as the Y
+  # plane.
 
   defp new_scaled_image(data, width, height, x_scale, y_scale)
       when x_scale == 1.0 and y_scale == 1.0 do
