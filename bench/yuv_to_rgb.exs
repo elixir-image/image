f644fdcc0013312468dfd2069c4ede40f370d0d1
Kip Cole <kipcole9@gmail.com>
2024-01-09 12:26:01 +0800

    Improve the benchmark descriptions

--- bench/yuv_to_rgb.exs
+++ bench/yuv_to_rgb.exs
@@ -1,12 +1,17 @@
 raw_yuv_file = Path.expand("./test/support/images/image.yuv")
 {:ok, binary} = File.read(raw_yuv_file)
+{:ok, image} = Image.YUV.new_from_binary(binary, 1920, 1080, :C420, :bt601)
 
 Benchee.run(
   %{
-    "Converting YUV binary to an RGB image" => fn ->
+    "Converting YUV 4:2:0 binary in BT601 to an RGB image" => fn ->
       {:ok, _i} = Image.YUV.new_from_binary(binary, 1920, 1080, :C420, :bt601)
     end,
-    },
+
+    "Converting an RGB image to YUV 4:2:0 binary in BT601" => fn ->
+      {:ok, _} = Image.YUV.write_to_binary(image, :C420, :bt601)
+    end,
+  },
   time: 20,
   memory_time: 2
 )
