8ec592c49ffc0b7a4f7f9764f6eedc273cace4a9
Kip Cole <kipcole9@gmail.com>
2024-07-03 19:14:50 +1000

    Increase test timeout

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,5 +4,5 @@ Enum.each(paths, &File.rm/1)
 Application.ensure_all_started(:telemetry)
 Application.ensure_all_started(:hackney)
 
-ExUnit.configure(exclude: [full: true])
+ExUnit.configure(exclude: [full: true], timeout: 120_000)
 ExUnit.start()
